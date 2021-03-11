77857045f0b0d04d64841ec5eb4f1544c9ce8da8
peterliao <peter@www.ucs.com>
2021-03-11 10:44:55 +0800

    test

--- Demo_Up2Github/ViewController.swift
+++ Demo_Up2Github/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("This demo app for upload to Github")
     }
 
 
